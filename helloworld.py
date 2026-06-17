c7abd1b79656a65804f8cac1961c058433b9f883
anki-xyz <11031615+anki-xyz@users.noreply.github.com>
2026-06-17 17:07:52 +0200

    day 2

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,16 @@ print("Hi!")
 x = 5
 x = 10
 y = 5
+
+def random(a:int, b:float) -> float:
+    """_summary_
+
+    Args:
+        a (int): The lower bound.
+        b (float): The upper bound.
+
+    Returns:
+        float: A random float between a and b.
+    """
+    import random
+    return random.uniform(a, b)
